c7a27c8ffe10251593a4ff74b8b1666386f4414b
taniguchi <rsb79692@gmail.com>
2026-04-29 08:57:24 +0900

    feat: PWA対応追加

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'timecard-v1';
+const OFFLINE_URLS = [
+  '/timecard/',
+  '/timecard/index.html',
+  '/timecard/manifest.json',
+  '/timecard/icon-192.png',
+  '/timecard/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(OFFLINE_URLS);
+    }).catch(function() {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
